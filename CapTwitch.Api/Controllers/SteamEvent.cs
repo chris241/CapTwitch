47be9dfbafee9c6b3dc69d19ac1c5222a3d2dd56
chris241 <chrisleon325@gmail.com>
2022-08-16 09:50:40 +0300

    first commit

---
+++ CapTwitch.Api/Controllers/SteamEvent.cs
@@ -0,0 +1,12 @@
+﻿namespace CapTwitch.Api.Controllers
+{
+    public class SteamEvent
+    {
+        public string Name { get; set; }
+        public string Description { get; set; } 
+        public DateTime Date { get; set; }
+        public string Background { get; set; }
+        public string Tecs { get; set; }
+
+    }
+}
