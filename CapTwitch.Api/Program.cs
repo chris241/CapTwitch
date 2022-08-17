73abaee32fbaa58a56682ec3c470efbb34900b75
chris241 <chrisleon325@gmail.com>
2022-08-17 13:24:20 +0300

    MAJ code add connexion string,

--- CapTwitch.Api/Program.cs
+++ CapTwitch.Api/Program.cs
@@ -1,10 +1,17 @@
+using CapTwitch.Api.Controllers;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContextPool<CaptTwitchDbContext>(op =>
+{
+
+});
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 
